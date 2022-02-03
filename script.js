d150c28d404d3c3ed3794ec6855586b1c7beba2d
radunicula <radu7.nicula@gmail.com>
2022-02-03 21:51:57 +0200

    add check, buttons and stop functions

--- script.js
+++ script.js
@@ -8,6 +8,49 @@ window.onload = function () {
     ];
     let word;
 
+    const buttons = function () {
+        let myButtons = document.getElementById('buttons');
+        let letters = document.createElement('ul');
+        for (let i = 0; i < alphabet.length; i++) {
+            letters.id = 'alphabet';
+            list = document.createElement('button');
+            list.id = 'letter';
+            list.setAttribute("class", "btn btn-dark")
+            list.setAttribute("style", "height:40px;width:35px")
+            list.innerHTML = alphabet[i];
+            check();
+            myButtons.appendChild(letters);
+            letters.appendChild(list);
+        }
+    };
+
+
+    let comments = function () {
+        showLives.innerHTML = "You have " + lives + " lives";
+        if (lives < 1) {
+            showLives.innerHTML = "Game Over";
+            stop();
+        }
+        for (let i = 0; i < geusses.length; ++i) {
+            if (counter + space === geusses.length) {
+                showLives.innerHTML = "You Win!";
+                win = 1;
+                stop();
+            }
+        }
+    }
+
+
+    let stop = function () {
+        let letters = document.getElementsByClassName("btn btn-dark");
+        if(win === 1 || lives === 0) {
+            for(let i = 0; i<letters.length;++i){
+                letters[i].disabled = true;
+            }
+        }
+    }
+
+
     let play = function () {
         word = words[Math.floor(Math.random() * words.length)];
         word = word.replace(/\s/g, "-");
